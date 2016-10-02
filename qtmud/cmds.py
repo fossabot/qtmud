cb8c732d1b47c036ced874daa291ff99b9f5a778
emsenn <morgan.sennhauser@gmail.com>
2016-10-01 23:54:20 -0400

    chg: doc: Documented Parameters in ``qtmud.cmds``

--- qtmud/cmds.py
+++ qtmud/cmds.py
@@ -4,15 +4,12 @@ import qtmud
 def commands(client, *, H=False, h=False):
     """ Sends a list of the client's commands to the client.
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) This isn't part of the command you enter.
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
 
-        Uses :func:`send <qtmud.subscriptions.send>` to send the client's
-        :attr:`qtmud.Client.commands` to themselves.
+        Creates a ``send`` event to tell the client a list of their commands.
     """
     output = ''
     brief = ('commands [-hH]\n\n'
@@ -32,12 +29,10 @@ def commands(client, *, H=False, h=False):
 def foo(client, *, h=False, H=False):
     """ The dedicated test command
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) **This isn't part of the command you enter.**
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
 
         This is a testing command, so you should check the source itself for
         information on what it actually does.
@@ -56,22 +51,20 @@ def foo(client, *, h=False, H=False):
     return True
 
 
-def help(client, topic=None, *, H=False, h=False, domain=None):
+def help(client, topic='', *, H=False, h=False, domain=''):
     """ The command for receiving in-game help. Searches through commands,
     subscribers, and active_services and returns either a list of available
     matching helpfiles, or the docstring of the single match.
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
-            topic (str):        None by default, otherwise the name of the
-                                command/service/subscriber you're looking to
-                                get help with.
-            domain (str):       Optionally limit where you're looking for help
-                                to either cmds, subscribers, or active_services.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) **This isn't part of the command you enter.**
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
+        :param topic:       None by default, otherwise the name of the
+                            command/service/subscriber you're looking to get
+                            help with.
+        :param domain:      Optionally limit where you're looking for help to
+                            either cmds, subscribers, or services.
 
         For example, you might enter ``help talker`` to learn more about qtMUD's
         talker system. You would be prompted to enter either
@@ -121,16 +114,14 @@ def help(client, topic=None, *, H=False, h=False, domain=None):
 def talker(client, channel=None, *, h=False, H=False, l=False):
     """ Command for interacting with the Talker service.
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
-            channel (str):      The channel you're getting information on.
-            l (bool):           Show the channel's history.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) **This isn't part of the command you enter.**
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
+        :param channel:     The channel you're getting information on.
+        :param l:           Show the channel's history.
 
-        .. todo:: --depth argument to look further back in history.
+        .. todo:: --depth argument to specify how much history to show
     """
     output = ''
     brief = ('syntax: foo [-hH]\n\n'
@@ -161,12 +152,10 @@ def talker(client, channel=None, *, h=False, H=False, l=False):
 def quit(client, *, H=False, h=False):
     """ Command to quit qtMUD
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) **This isn't part of the command you enter.**
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
     """
     output = ''
     brief = ('syntax: help [-hH]\n\n'
@@ -186,12 +175,10 @@ def quit(client, *, H=False, h=False):
 def who(client, *, H=False, h=False):
     """ Command to quit qtMUD
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) **This isn't part of the command you enter.**
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
     """
     output = ''
     brief = ('syntax: who [-hH]\n\n'
@@ -211,12 +198,10 @@ def who(client, *, H=False, h=False):
 def whoami(client, *, H=False, h=False):
     """ Command to quit qtMUD
 
-        Parameters:
-            client (object):    The client issuing the foo command. (That'd be
-                                you.) **This isn't part of the command you
-                                enter.**
-            h (bool):           Shows the client a brief help.
-            H (bool):           Shows the client this docstring.
+        :param client:      The client issuing the foo command. (That'd be
+                            you.) **This isn't part of the command you enter.**
+        :param h:           Shows the client a brief help.
+        :param H:           Shows the client this docstring.
     """
     output = ''
     brief = ('syntax: who [-hH]\n\n'
