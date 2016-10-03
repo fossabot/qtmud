53fefd06f42dc20e31426d347974d282c83df699
emsenn <morgan.sennhauser@gmail.com>
2016-10-03 03:04:48 -0400

    new: usr: Addition Of `tell` Client Command

--- qtmud/cmds.py
+++ qtmud/cmds.py
@@ -165,6 +165,42 @@ def talker(client, channel=None, *, H=False, h=False, l=False):
         return True
 
 
+def tell(client, recipient='', message='', H=False, h=False):
+    """ Command for interacting with the Talker service.
+
+        :param client:      The client issuing the command. (That'd be you.)
+                            **This isn't part of the command you enter.**
+        :param H:           Shows the client this docstring.
+        :param h:           Shows the client a brief help.
+    """
+    output = ''
+    brief = ('syntax: tell [-Hh] <recipient> <message>\n\n'
+             'Sends `message` to `recipient`.')
+    if H:
+        output += talker.__doc__
+    elif h:
+        output = brief
+    else:
+        recipients = qtmud.search_connected_clients_by_name(recipient)
+        if len(recipients) == 1:
+            recipient = recipients[0]
+            if client == recipient:
+                output += 'You tell yourself: {}'.format(message)
+            else:
+                output += 'You tell {}: {}'.format(recipient.name, message)
+                qtmud.schedule('send',
+                               recipient=recipient,
+                               text='`{}` tells you: {}'.format(client.name,
+                                                                message))
+        elif len(recipients) > 1:
+            output += ('More than one match: {}'
+                       ''.format(', '.join([r.name for r in recipients])))
+        else:
+            output += ('Couldn\'t find a player with the name `{}`'
+                       ''.format(recipient))
+    if output:
+        qtmud.schedule('send', recipient=client, text=output)
+
 def quit(client, *, H=False, h=False):
     """ Command to quit qtMUD
 
